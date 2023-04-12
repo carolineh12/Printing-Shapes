bea96827e0c6f8669d7d4b3a3816ea478b4ab167
liz-denson <123184723+liz-denson@users.noreply.github.com>
2023-04-12 17:01:38 -0400

    Update Program 5.py

--- Program 5.py
+++ Program 5.py
@@ -1,73 +1,88 @@
-#parallelogram; if have three rows, shift first one over 2, second one over 1, third one no shift
-#constructor; if <=0 make it equal to 1
-#if p1.width = -1, keep the width at what it was already set at(ignore)   
+####################################################################
+# Shapes
+# Liz Denson & Caroline Holland
+# Last modified on 2023-04-12
+#
+# An object-oriented program that implements inheritance.
+####################################################################
 
+# Shape class
 class Shape:
-    def __init__(self, w=1, h=1):
-        self.width = w
-        self.height = h
     
-    @property 
+    # Initializes the Shape class with width and height parameters
+    def __init__(self, w=1, h=1):
+        self._width = w if w > 0 else 1
+        self._height = h if h > 0 else 1
+        
+    # Width getter
+    @property
     def width(self): 
         return self._width
+    
+    # Width setter
     @width.setter
     def width(self, w):
-        if w <= 0:
-            self._width = 1
-        else:
+        if w > 0:
             self._width = w
+            
+    # Height getter
     @property 
     def height(self): 
         return self._height
+    
+    # Height setter
     @height.setter
     def height(self, h):
-        if h <= 0:
-            self._height = 1
-        else:
+        if h > 0:
             self._height = h
+            
+    # __str__ method to view the shape as a string
     def __str__(self):
         image = ""
         for i in range(self.height):
             image += ("* " * self.width) + "\n"
-        return image 
-        
+        return image
+    
+# Rectangle class inheriting from Shape
 class Rectangle(Shape):
     def __init__(self, w, h):
         super().__init__(w, h)
         
+# Square class inheriting from Shape
 class Square(Shape):
     def __init__(self, w):
-        super().__init__(w, w) #passing in l for l and w in the shape constructor 
-
+        super().__init__(w, w) # Passes w as both width and height in the Shape constructor
+        
+# Triange class inheriting from Shape
 class Triangle(Shape):
     def __init__(self, w):
         super().__init__(w, w)
-    
+        
+    # __str__ method to view the Triangle as a string
     def __str__(self):
         image = ""
         for i in range(self.height):
             image += ("* " * (self.height - i)) + "\n"
         return image
-
+    
+# Parallelogram class inheriting from Shape
 class Parallelogram(Shape):
     def __init__(self, w, h):
-        super().__init__(w, h)
-    
+        super().__init__(w, h) # Calls the Shape constructor
+        
+    # __str__ method to view the Parallelogram as a string
     def __str__(self):
         image = ""
         shift = self.height 
         for i in range(self.height):
             image += "  " * shift + "* " * (self.width) + "\n"
-            shift -=1
-        return image 
-
-        
-        
+            shift -= 1
+        return image
     
+###############
+# MAIN PROGRAM
+###############
 
-
-
-#MAIN PROGRAM
 r1 = Rectangle(12, 4)
 print(r1)
 s1 = Square(6)
@@ -82,4 +97,3 @@ p1.width = 2
 p1.width = -1
 p1.height = 2
 print(p1)
-
